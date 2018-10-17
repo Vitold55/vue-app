f15803ae9b0587ea8c4cbe65e97c47dfff4a696c
Vitold55 <viktor@privatix.io>
2018-10-17 12:41:33 +0300

    Change form size on lg screens

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -1,7 +1,7 @@
 <template>
     <v-container fluid fill-height>
         <v-layout align-center justify-center>
-            <v-flex xs12 sm8 md6>
+            <v-flex xs12 sm8 md6 lg4>
                 <v-card class="elevation-12">
                     <v-toolbar dark color="primary">
                         <v-toolbar-title>Registration form</v-toolbar-title>
